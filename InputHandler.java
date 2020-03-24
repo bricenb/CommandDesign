3feebb0c1ad969a03ba0213f92bb58fc151da26c
U-DESKTOP-GBRVOVO\Thomas <thomasjjervey@gmail.com>
2020-03-24 00:50:16 -0400

    Implemented proper hashmap implementation

--- InputHandler.java
+++ InputHandler.java
@@ -1,33 +1,27 @@
 /**
  * Jackson, Bricen, Thomas, John
- * this class Holds a hashmap for the commands, and their input keys
+ * This class holds a hashmap for the commands, and their input keys
  */
 import java.util.HashMap;
 
 public class InputHandler {
-    private HashMap<String, String> commands;
-
-
-    public InputHandler(Document doc) {
-        commands = new HashMap<>();
-
-        commands.put("load"," document is loading into view");
-        commands.put("save","document is being saved");
-        commands.put("spell check","document is being checked being checked for spelling errors");
-        commands.put("print", "document is printing");
+    private HashMap<String, Command> commands;
+    
+    public InputHandler(Document document) {
+        commands = new HashMap<String, Command>();
+        
+        commands.put("load", new LoadCommand(document));
+        commands.put("save", new SaveCommand(document));
+        commands.put("spell", new SpellCommand(document));
+        commands.put("print", new PrintCommand(document));
     }
-
+    
+    //Check if the key is valid then execute the command
     public void inputEntered(String data) {
-        if (data.equals("load")){
-            System.out.println(commands.get("load"));
-        } else if (data.equals("save")){
-            System.out.println(commands.get("save"));
-        } else if (data.equals("spell check")) {
-            System.out.println(commands.get("spell check"));
-        } else if (data.equals("print")) {
-            System.out.println(commands.get("print"));
-        } else {
-            System.out.println("Please enter another command, that one does not exist");
+    	if (commands.containsKey(data)) {
+    		commands.get(data).execute();
+    	} else {
+            System.out.println("Sorry, we don't recognize that command");
         }
     }
 }
